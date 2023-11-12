7f729f00afcc0196ef85dfe09bb1034b73265d5a
FH3095 <FH3095@users.noreply.github.com>
2023-11-12 19:11:46 +0100

    Update to Java 17 / Jakarta

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,18 +12,24 @@ allprojects {
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_11
-	targetCompatibility = JavaVersion.VERSION_11
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
-	implementation("com.github.spotbugs:spotbugs-annotations:4.7.3")
-	implementation("org.json:json:20220924")
-	implementation("org.apache.httpcomponents.core5:httpcore5:5.1.4")
-    implementation("org.glassfish.jersey.core:jersey-client:2.37")
-    implementation("org.glassfish.jersey.media:jersey-media-json-jackson:2.37")
-    api("org.dmfs:oauth2-essentials:0.18")
-    implementation("org.dmfs:httpurlconnection-executor:0.20")
+	implementation("com.github.spotbugs:spotbugs-annotations:4.8.1")
+	implementation("org.json:json:20231013")
+	implementation("org.apache.httpcomponents.core5:httpcore5:5.2.3")
+    implementation("org.glassfish.jersey.core:jersey-client:3.1.3")
+    implementation("org.glassfish.jersey.media:jersey-media-json-jackson:3.1.3")
+    api("org.dmfs:oauth2-essentials:0.22.1")
+    val httpVersion = "1.21.3"
+    implementation("org.dmfs:http-client-essentials:${httpVersion}")
+    implementation("org.dmfs:http-client-basics:${httpVersion}")
+    implementation("org.dmfs:http-client-headers:${httpVersion}")
+    implementation("org.dmfs:http-executor-decorators:${httpVersion}")
+    implementation("org.dmfs:http-client-types:${httpVersion}")
+    implementation("org.dmfs:httpurlconnection-executor:${httpVersion}")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
